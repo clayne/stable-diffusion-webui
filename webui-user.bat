91effd04b366933edd2630dfa94a985f52a00134
Christopher Layne <clayne@anodized.com>
2024-02-21 16:18:11 -0800

    local edits

--- webui-user.bat
+++ webui-user.bat
@@ -3,6 +3,9 @@
 set PYTHON=
 set GIT=
 set VENV_DIR=
-set COMMANDLINE_ARGS=
 
-call webui.bat
+set COMMANDLINE_ARGS=--loglevel=INFO --log-startup --no-download-sd-model --do-not-download-clip --xformers --no-half-vae --opt-channelslast
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.9,max_split_size_mb:1024
+set NUMEXPR_MAX_THREADS=16
+
+call webui.bat < nul
